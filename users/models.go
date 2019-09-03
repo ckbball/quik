868584a28dbdbf37ded0f23ff50d1fa8a8a4e6a0
Caesar Ladion <caesar.ladion@gmail.com>
2019-09-03 16:44:33 -0700

    deciding if i should build out lower level stuff to grab arrays of info or to try and use gorm based on golang-gin realworld example

--- users/models.go
+++ users/models.go
@@ -13,12 +13,12 @@ type UserModel struct {
   Email     string `json:"email"`
   Hash      string `json:"pass"`
   HasInfo   bool   `json:"info"`
+  Status     string   // this is going to be searching, perusing, locked
+  Level      string   // this is going to be entry, mid, senior
   // Info      UserInfo `json:"info"`  should this be in UserModel or should it just be a separate table that I also grab
 }
 
 type UserInfo struct {
-  Status     string   // this is going to be searching, perusing, locked
-  Level      string   // this is going to be entry, mid, senior
   Roles      []string // this is going to be backend, frontend, full stack, mobile,
   Frameworks []string // this is going to be all frameworks, front and back, that user knows meaning they built a project with it
   DB         []string // this is all dbs that a user has built a project with
@@ -27,8 +27,10 @@ type UserInfo struct {
   Extra      []string // dont know what should be here
   DevOps     []string // CI/CD tools, other things idk
   Cloud      []string // which cloud tools and platforms user has made a project with
-  UserId     int      // the id that this info block belongs to
+  ID     int      
 }
+
+// There will be multiple relations table with id, user_id, <a field of userInfo>_id, 
 
 // -------- HELPER FUNCTIONS BEGIN --------------------------------------
 
@@ -41,6 +43,8 @@ func UserValidatorToModel(newUser UserModelValidator) (UserModel, error) {
   u.Email = newUser.User.Email
   u.Hash = newUser.User.Hash
   u.HasInfo = newUser.User.HasInfo
+  u.Level = newUser.User.Level
+  u.Status = newUser.User.Status
 
   return u, nil
 }
@@ -53,9 +57,11 @@ func scanUsers(s *sql.Rows) (*UserModel, error) {
     Email     sql.NullString
     Hash      sql.NullString
     HasInfo   sql.NullBool
+    Status    sql.NullString
+    Level     sql.NullString
   )
 
-  if err := s.Scan(&ID, &Firstname, &Lastname, &Email, &Hash, &HasInfo); err != nil {
+  if err := s.Scan(&ID, &Firstname, &Lastname, &Email, &Hash, &HasInfo, &Status, &Level); err != nil {
     return nil, err
   }
 
@@ -68,6 +74,8 @@ func scanUsers(s *sql.Rows) (*UserModel, error) {
     Email:     Email.String,
     Hash:      Hash.String,
     HasInfo:   HasInfo.String,
+    Level:     Level.String,
+    Status:    Status.String
   }
 
   return user, nil
@@ -81,9 +89,11 @@ func scanUser(s *sql.Row) (*UserModel, error) {
     Email     sql.NullString
     Hash      sql.NullString
     HasInfo   sql.NullBool
+    Status    sql.NullString
+    Level     sql.NullString
   )
 
-  if err := s.Scan(&ID, &Firstname, &Lastname, &Email, &Hash, &HasInfo); err != nil {
+  if err := s.Scan(&ID, &Firstname, &Lastname, &Email, &Hash, &HasInfo, &Status, &Level); err != nil {
     return nil, err
   }
 
@@ -96,33 +106,27 @@ func scanUser(s *sql.Row) (*UserModel, error) {
     Email:     Email.String,
     Hash:      Hash.String,
     HasInfo:   HasInfo.String,
+    Level:      Level.String,
+    Status:     Status.String
   }
 
   return user, nil
 }
 
 func scanUserInfos(s *sql.Rows) (*UserModel, error) {
-  Status     string   // this is going to be searching, perusing, locked
-  Level      string   // this is going to be entry, mid, senior
-  Roles      []string // this is going to be backend, frontend, full stack, mobile,
-  Frameworks []string // this is going to be all frameworks, front and back, that user knows meaning they built a project with it
-  DB         []string // this is all dbs that a user has built a project with
-  Front      []string // languages and methods for front end user has built a project with
-  Back       []string // languages and methods for backend user has built a project with
-  Extra      []string // dont know what should be here
-  DevOps     []string // CI/CD tools, other things idk
-  Cloud      []string // which cloud tools and platforms user has made a project with
-  UserId     int      // the id that this info block belongs to
   var (
-    UserID        int
-    Status sql.NullString
-    Level  sql.NullString
-    Email     sql.NullString
-    Hash      sql.NullString
-    HasInfo   sql.NullBool
+    ID     int
+    Roles      sql.JSON
+    Frameworks sql.JSON
+    DB         sql.JSON
+    Front      sql.JSON
+    Back       sql.JSON
+    Extra      sql.JSON
+    DevOps     sql.JSON
+    Cloud      sql.JSON
   )
 
-  if err := s.Scan(&ID, &Firstname, &Lastname, &Email, &Hash, &HasInfo); err != nil {
+  if err := s.Scan(&ID, &Roles, &Frameworks, &DB, &Front, &Back, &Extra, &DevOps, &Cloud); err != nil {
     return nil, err
   }
 
@@ -130,11 +134,13 @@ func scanUserInfos(s *sql.Rows) (*UserModel, error) {
 
   user := &UserModel{
     ID:        id,
-    Firstname: Firstname.String,
-    Lastname:  Lastname.String,
-    Email:     Email.String,
-    Hash:      Hash.String,
-    HasInfo:   HasInfo.String,
+    Status: Status.String,
+    Level:  Level.String,
+    Roles:     Roles.Slice,
+    Frameworks:      Frameworks.Slice,
+    DB:   DB.String,
+    Front: Front.
+
   }
 
   return user, nil
@@ -193,7 +199,7 @@ func GetAllUsers() ([]*UserModel, error) {
   return users, nil
 }
 
-func SaveFlight(u UserModel) error {
+func SaveUser(u UserModel) error {
 
   r, err := common.ExecAffectingOneRow("insert", "user", u.ID, u.Firstname, u.Lastname, u.Email, u.Hash, u.HasInfo)
   if err != nil {
@@ -202,6 +208,20 @@ func SaveFlight(u UserModel) error {
   fmt.Println("Insert user operation results: %v", r)
 
   return nil
+}
+
+func GetUser(id int) (*UserModel, error) {
+  row, err := common.GetRow("users", id)
+  if err != nil {
+    return nil, err
+  }
+
+  user, err := scanUser(row)
+  if err != nil {
+    return nil, fmt.Errorf("ERROR --->> mysql: could not read row: %v", err)
+  }
+
+  return flight, nil
 }
 
 // ----------------- DB FUNCTIONS END ------------------------------
