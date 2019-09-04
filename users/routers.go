e57e7722ce63f2d884fc6a274898789c10a5252f
Caesar Ladion <caesar.ladion@gmail.com>
2019-09-04 13:08:40 -0700

    register

--- users/routers.go
+++ users/routers.go
@@ -6,3 +6,8 @@ import (
   "github.com/gin-gonic/gin"
   "net/http"
 )
+
+func UsersRegister(router *gin.RouterGroup) {
+  router.POST("/", UsersRegistration)
+  router.POST("/login", UsersLogin)
+}
