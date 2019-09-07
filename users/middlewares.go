256dd43568f7ed8b7c327aa23ba6fe23e436e987
Caesar Ladion <caesar.ladion@gmail.com>
2019-09-06 18:39:39 -0700

    added auth helper funcs

--- users/middlewares.go
+++ users/middlewares.go
@@ -2,7 +2,10 @@ package users
 
 import (
   "github.com/ckbball/quik/common"
+  "github.com/dgrijalva/jwt-go"
+  "github.com/dgrijalva/jwt-go/request"
   "github.com/gin-gonic/gin"
+  "net/http"
 )
 
 func UpdateContextUserModel(c *gin.Context, my_user_id int) {
@@ -13,4 +16,44 @@ func UpdateContextUserModel(c *gin.Context, my_user_id int) {
   }
   c.Set("my_user_id", my_user_id)
   c.Set("my_user_model", user)
+}
+
+func AuthMiddleware(auto401 bool) gin.HandlerFunc {
+  return func(c *gin.Context) {
+    UpdateContextUserModel(c, 0)
+    token, err := request.ParseFromRequest(c.Request, MyAuth2Extractor, func(token *jwt.Token) (interface{}, error) {
+      b := ([]byte(common.JWTSecretString))
+    })
+    if err != nil {
+      if auto401 {
+        c.AbortWithError(http.StatusUnauthorized, err)
+      }
+      return
+    }
+    if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+      my_user_id := uint(claims["id"].(float))
+      UpdateContextUserModel(c, my_user_id)
+    }
+  }
+}
+
+func stripBearerPrefixFromTokenString(tok string) (string, error) {
+  if len(tok) > 5 && strings.ToUpper(tok[0:6]) == "TOKEN " {
+    return tok[6:], nil
+  }
+  return tok, nil
+}
+
+// Extract  token from Authorization header
+// Uses PostExtractionFilter to strip "TOKEN " prefix from header
+var AuthorizationHeaderExtractor = &request.PostExtractionFilter{
+  request.HeaderExtractor{"Authorization"},
+  stripBearerPrefixFromTokenString,
+}
+
+// Extractor for OAuth2 access tokens.  Looks in 'Authorization'
+// header then 'access_token' argument for a token.
+var MyAuth2Extractor = &request.MultiExtractor{
+  AuthorizationHeaderExtractor,
+  request.ArgumentExtractor{"access_token"},
 }
