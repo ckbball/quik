95d4311d2add87e57d4498e0a65ae6ef797c749b
Caesar Ladion <caesar.ladion@gmail.com>
2019-09-06 18:41:35 -0700

    bugfix

--- users/middlewares.go
+++ users/middlewares.go
@@ -24,6 +24,7 @@ func AuthMiddleware(auto401 bool) gin.HandlerFunc {
     UpdateContextUserModel(c, 0)
     token, err := request.ParseFromRequest(c.Request, MyAuth2Extractor, func(token *jwt.Token) (interface{}, error) {
       b := ([]byte(common.JWTSecretString))
+      return b, nil
     })
     if err != nil {
       if auto401 {
@@ -32,7 +33,7 @@ func AuthMiddleware(auto401 bool) gin.HandlerFunc {
       return
     }
     if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-      my_user_id := uint(claims["id"].(float64))
+      my_user_id := int(claims["id"].(float64))
       UpdateContextUserModel(c, my_user_id)
     }
   }
