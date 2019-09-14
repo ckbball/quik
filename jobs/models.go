9bb927ae2f5f9fefd535480f2fcf7a0179e01659
Caesar Ladion <caesar.ladion@gmail.com>
2019-09-14 14:00:52 -0700

    starting create a job. have to do new auth for it

--- jobs/models.go
+++ jobs/models.go
@@ -1,17 +1,36 @@
 package jobs
 
 import (
-//"errors"
-//"fmt"
-//"github.com/ckbball/quik/common"
-//"github.com/jinzhu/gorm"
-//"golang.org/x/crypto/bcrypt"
+  //"errors"
+  //"fmt"
+  "github.com/ckbball/quik/common"
+  //"github.com/jinzhu/gorm"
+  //"golang.org/x/crypto/bcrypt"
 )
 
 // unsure about this model
 type JobModel struct {
-  ID               int `gorm:"primary_key"`
-  CompanyID        int
+  gorm.Model
+  CompanyID        int    `gorm:"column:company_id"`
   Responsibilities string `gorm:"column:responsibilities"`
-  Skills           string `gorm:"string"`
+  Skills           string `gorm:"column:skills"`
+}
+
+func AutoMigrate() {
+  db := common.GetDB()
+
+  db.AutoMigrate(&JobModel{})
+}
+
+func SaveOne(data interface{}) error {
+  db := common.GetDB()
+  err := db.Save(data).Error
+  return err
+}
+
+func FindOneCompany(condition interface{}) (CompanyModel, error) {
+  db := common.GetDB()
+  var model CompanyModel
+  err := db.Where(condition).First(&model).Error
+  return model, err
 }
